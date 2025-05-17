55538c69d7fef62a5bc846f58f0b9f32feef87b7
Rayeesa Mahmood <151394585+RayeesaMahmood@users.noreply.github.com>
2025-05-18 01:32:56 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,27 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBo0BpS3IhefRS7Wj1-HHnmWeSMB5brstc",
+    authDomain: "raycare-908e9.firebaseapp.com",
+    databaseURL: "https://raycare-908e9-default-rtdb.firebaseio.com",
+    projectId: "raycare-908e9",
+    storageBucket: "raycare-908e9.firebasestorage.app",
+    messagingSenderId: "712230082136",
+    appId: "1:712230082136:web:d9b2c1dce84a979b4e9c2d",
+    measurementId: "G-3V8E80S9KX"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  //emergency data
+  
+</script>
